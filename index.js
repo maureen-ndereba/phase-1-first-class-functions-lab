82cc74441960e20a062f068777c03a5c9493dab6
mojustmo-glitch <maureen.ngugi@student.moringaschool.com>
2023-03-31 00:32:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-// Code your solution in this file!
+const returnFirstTwoDrivers = function (driver) { return [driver[0], driver[1] ] };
+
+const returnLastTwoDrivers = function (driver) { return [driver[driver.length-2], driver[driver.length-1] ] };
+
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(){
+ return function (x) {return (x * x)}
+
+}
+
+const myDoubles = createFareMultiplier()
+
+function fareDoubler(y) {
+return (myDoubles(y)/y)*2
+}
+
+const myTripple = createFareMultiplier()
+
+function fareTripler(y) {
+return (myTripple(y)/y)*3
+}
+
+function selectDifferentDrivers(myDrivers, driverSelector){
+return driverSelector(myDrivers)
+}
